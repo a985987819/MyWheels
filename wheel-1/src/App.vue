8dec858018936b2fa382b0fb697d53e4a0262f11
蛋蛋怪将军 <985987819@qq.com>
2021-01-12 18:49:25 +0800

    初始化了项目

---
+++ wheel-1/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hi</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Laoduan from './components/Laoduan'
+export default class App extends Vue{
+  components: {
+    HelloWorld,
+    Laoduan:Laoduan
+  }
+}
+</script>
