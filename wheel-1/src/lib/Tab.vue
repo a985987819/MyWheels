f1457ae5e637f79679be4fe386264f3dd8643c81
蛋蛋怪将军 <985987819@qq.com>
2021-01-19 18:09:21 +0800

    实现切换Tabs功能

--- wheel-1/src/lib/Tab.vue
+++ wheel-1/src/lib/Tab.vue
@@ -1,19 +1,5 @@
 <template>
     <div>
-        
+        <slot/>
     </div>
 </template>
-
-<script lang="ts">
-    import Vue from 'vue';
-    import {Component} from 'vue-property-decorator';
-
-    @Component
-    export default class Tab extends Vue {
-        name: '';
-    }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
