8e95b346d8b483a4b8e8e3c6ebc0194fd778fc0d
蛋蛋怪将军 <985987819@qq.com>
2021-01-19 17:11:23 +0800

    创建Tabs和Tab组件

--- wheel-1/src/components/DialogDemo.vue
+++ wheel-1/src/components/DialogDemo.vue
@@ -2,8 +2,8 @@
     <div>Dialog示例
     </div>
     <h1>示例1</h1>
-    <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visible="x"
+    <Button @click="toggle">召唤Dialog</Button>
+    <Dialog v-model:visible="visible"
             :CloseOnClickOverlay="false"
             :ok="fn1"
             :cancel="fn2">
@@ -17,18 +17,21 @@
             <div>被怀疑刷单</div>
         </template>
     </Dialog>
+    <h1>示例2</h1>
+    <Button @click="showDialog">显示dialog</Button>
 </template>
 <script lang="ts">
     import Dialog from '../lib/Dialog.vue';
     import Button from '../lib/Button.vue';
+    import {openDialog} from '../lib/openDialog';
     import {ref} from 'vue';
 
     export default {
         components: {Button, Dialog},
         setup() {
-            const x = ref(false);
+            const visible = ref(false);
             const toggle = () => {
-                x.value = !x.value;
+                visible.value = !visible.value;
             };
             const fn1 = () => {
 
@@ -36,8 +39,21 @@
             const fn2 = () => {
 
             };
+
+            const showDialog = () => {
+                openDialog({
+                    title: '警告',
+                    content: '不能再吃了',
+                    ok() {
+                        console.log('ok');
+                    },
+                    cancel() {
+                        console.log('cancel');
+                    },
+                });
+            };
             return {
-                x, toggle, fn1, fn2
+                visible, toggle, fn1, fn2, showDialog,
             };
         }
     };
