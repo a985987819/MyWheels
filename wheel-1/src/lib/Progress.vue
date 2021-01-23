c5790f33101f4af751b68eacd08f6cd195a17df4
蛋蛋怪将军 <985987819@qq.com>
2021-01-23 15:14:55 +0800

    优化Slider样式

---
+++ wheel-1/src/lib/Progress.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="gulu-progress"
+         :style="progressStyle"
+    ></div>
+    <div class="container"
+         :style="progressStyle1"
+    >
+        <button>
+        </button>
+    </div>
+
+</template>
+<script lang="ts">
+    import {ref, watchEffect, reactive} from 'vue';
+
+    export default {
+        props: {
+            myvalue: {
+                type: Number,
+                default: 0
+            }
+        },
+        setup(props) {
+            let progressStyle = reactive({
+                width: '200px',
+            });
+            let progressStyle1 = reactive({
+                width: '200px',
+            });
+            watchEffect(
+                () => {
+                    progressStyle1.width = (props.myvalue * 3) + 'px';
+                    progressStyle.width = (props.myvalue * 3) + 'px';
+                }
+            );
+            return {progressStyle};
+        },
+    };
+</script>
+<style lang="scss">
+    .gulu-progress {
+        background: #0d92ff;
+        border-radius: 2px;
+        height: 4px;
+        width: 100px;
+        position: relative;
+        margin-top: 5px;
+    }
+
+    .container {
+        position: absolute;
+        margin-top: -17px;
+
+        > button {
+            position: relative;
+            width: 12px;
+            height: 12px;
+            border-radius: 50%;
+            background: #ffffff;
+            border: 1px solid #0d92ff;
+        }
+    }
+
+
+    .value {
+        margin-bottom: 0;
+        margin-top: -10px;
+        margin-left: 110%;
+        font-size: 16px;
+        width: 20px;
+        height: 20px;
+        color: white;
+        font-weight: bold;
+    }
+</style>
---
+++ wheel-1/src/views/Home.vue
@@ -0,0 +1,123 @@
+<template>
+    <div>
+        <div class="TopnavAndBanner">
+            <Topnav/>
+            <div class="banner">
+                <h1>0UI</h1>
+                <h2>一个有点厉害的UI框架</h2>
+                <p class="actions">
+                    <a href="https://github.com">Github</a>
+                    <router-link to="/Doc">开始</router-link>
+                </p>
+            </div>
+        </div>
+        <div class="features">
+            <ul>
+                <li>
+                    <svg>
+                        <use xlink:href="#icon-Vue"></use>
+                    </svg>
+                    <h3>基于 Vue 3</h3>
+                    <p>骄傲地使用了 Vue 3 Composition API</p>
+                </li>
+                <li>
+                    <svg>
+                        <use xlink:href="#icon-ts"></use>
+                    </svg>
+                    <h3>基于 TypeScript </h3>
+                    <p>源代码采用 TypeScript 书写（非严格检查）</p>
+                </li>
+                <li>
+                    <svg>
+                        <use xlink:href="#icon-idea"></use>
+                    </svg>
+                    <h3>代码易读</h3>
+                    <p>每个组件的源代码都极其简洁</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</template>
+<style lang="scss" scoped>
+    $green: #29928d;
+    $border-radius: 6px;
+    $color: #00a5a0;
+    .TopnavAndBanner {
+        background: linear-gradient(135deg, rgba(227, 252, 250, 1) 0%, rgba(120, 227, 216, 1) 100%);
+        clip-path: ellipse(80% 60% at 50% 40%);
+    }
+    .features{
+        margin: 64px auto;
+        width: 400px;
+        @media (min-width: 800px) {
+            width: 800px;
+        }
+        @media (min-width: 1200px) {
+            width: 1200px;
+        }
+        >ul {
+            display: flex;
+            flex-wrap: wrap;
+
+            > li {
+                width: 400px;
+                margin: 16px 0;
+                display: grid;
+                justify-content: start;
+                align-content: space-between;
+                grid-template-areas: "icon title" "icon text";
+                grid-template-columns: 80px auto;
+                grid-template-rows: 1fr auto;
+
+                > svg {
+                    grid-area: icon;
+                    width: 64px;
+                    height: 64px;
+                }
+
+                > h3 {
+                    grid-area: title;
+                    font-size: 28px;
+                }
+
+                > p {
+                    grid-area: text
+                }
+            }
+        }
+    }
+
+    .banner {
+        color: $color;
+        padding: 100px 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        > .actions {
+            padding: 8px 0;
+
+            a {
+                margin: 0 8px;
+                background: $green;
+                color: white;
+                display: inline-block;
+                padding: 8px 24px;
+                border-radius: $border-radius;
+
+                &:hover {
+                    text-decoration: none;
+                }
+            }
+        }
+    }
+</style>
+<script lang="ts">
+    import Topnav from '../components/Topnav.vue';
+
+    export default {
+        components: {Topnav}
+    };
+</script>
